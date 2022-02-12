19b25698ae0d43c14cedcfd56059943e0cb59c7a
McBarbar <42481948+StanMc@users.noreply.github.com>
2022-02-12 07:26:16 -0800

    adding items to the list is working, but doesn't display yet.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [toDoItemList, setToDoItemList] = useState(["paint wall", "dust"]);
+
+  function inputOnChangeHandler(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function buttonAddHandler(event) {
+    setToDoItemList((prevValue) => {
+      return [...prevValue, inputText];
+    });
+  }
+
+  function buttonConsoleLogHandler(event) {
+    console.log(toDoItemList);
+    console.log(inputText);
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={inputOnChangeHandler} value={inputText} type="text" />
+        <button onClick={buttonAddHandler}>
           <span>Add</span>
         </button>
       </div>
@@ -17,6 +37,9 @@ function App() {
           <li>A Item </li>
         </ul>
       </div>
+      <button onClick={buttonConsoleLogHandler}>
+        <span> Console Log </span>
+      </button>
     </div>
   );
 }
